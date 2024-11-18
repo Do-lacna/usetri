b32bba7d73878032fa7a700030dc8a3410c74c5b
Adam Martiska <adam.martiska@kistler.com>
2024-11-18 23:10:55 +0100

    Add initial firebase files for startup

---
+++ firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-bsRkMTX6GYvp7CNEiqkcWEJsIhtdiec",
+  authDomain: "dolacna-388d4.firebaseapp.com",
+  projectId: "dolacna-388d4",
+  storageBucket: "dolacna-388d4.appspot.com",
+  messagingSenderId: "504961053140",
+  appId: "1:504961053140:android:a31a3f46a792c807fbe641",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
