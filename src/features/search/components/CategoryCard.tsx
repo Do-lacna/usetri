b50215c0bfba1dd12c9f9e7d003fdc5226681531
Adam Martiska <adam.martiska@kistler.com>
2025-11-25 21:42:13 +0100

    Use Expo Image for CategoryCard and add BlurHash for better UX

--- src/features/search/components/CategoryCard.tsx
+++ src/features/search/components/CategoryCard.tsx
@@ -1,7 +1,11 @@
-import { Image, Platform, Pressable, View } from 'react-native';
-import { PLACEHOLDER_PRODUCT_IMAGE } from '../../../lib/constants';
-import type { PopularCategoryDto } from '~/src/network/model';
+import { Image } from 'expo-image';
+import { cssInterop } from 'nativewind';
+import { Platform, Pressable, View } from 'react-native';
 import { Text } from '~/src/components/ui/text';
+import type { PopularCategoryDto } from '~/src/network/model';
+import { PLACEHOLDER_PRODUCT_IMAGE } from '../../../lib/constants';
+
+cssInterop(Image, { className: 'style' });
 
 interface CategoryCardProps {
   category: PopularCategoryDto;
@@ -28,7 +32,14 @@ export function CategoryCard({ category, onPress }: CategoryCardProps) {
             uri: category?.category?.image_url ?? PLACEHOLDER_PRODUCT_IMAGE,
           }}
           className="w-full h-full absolute inset-0"
-          resizeMode="cover"
+          contentFit="cover"
+          transition={400}
+          cachePolicy="memory-disk"
+          priority="high"
+          placeholder={{
+            blurhash:
+              '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[',
+          }}
         />
 
         <View className="absolute bottom-0 left-0 right-0 bg-card/75 backdrop-blur-sm px-2 py-3">
